f3ba02ed2d864b34e597613ca77dd38d9b65fd2c
MEMUZ <mitigdte@mail.ru>
2026-03-11 18:47:31 +0300

    refactor(flags): more robust flags parsing

--- cli/flags.go
+++ cli/flags.go
@@ -11,31 +11,22 @@ type Config struct {
 }
 
 func ParseFlags() Config {
-	dryRun := flag.Bool("dry-run", false, "preview sorting without moving files")
-	quiet := flag.Bool("quiet", false, "show only final statistics")
-	ignore := flag.String("ignore", "", "comma separated list of files or extensions to ignore")
-	recursive := flag.Bool("recursive", false, "sort files recursively")
+	cfg := Config{}
 
-	dryRunShort := flag.Bool("d", false, "dry-run (short)")
-	quietShort := flag.Bool("q", false, "quiet mode (short)")
-	ignoreShort := flag.String("i", "", "ignore files (short)")
-	recursiveShort := flag.Bool("r", false, "recursive (short)")
+	flag.BoolVar(&cfg.DryRun, "dry-run", false, "preview sorting without moving files")
+	flag.BoolVar(&cfg.Quiet, "quiet", false, "show only final statistics")
+	flag.StringVar(&cfg.Ignore, "ignore", "", "comma separated list of files or extensions to ignore")
+	flag.BoolVar(&cfg.Recursive, "recursive", false, "sort files recursively")
+
+	flag.BoolVar(&cfg.DryRun, "d", false, "dry-run (short)")
+	flag.BoolVar(&cfg.Quiet, "q", false, "quiet mode (short)")
+	flag.StringVar(&cfg.Ignore, "i", "", "ignore files (short)")
+	flag.BoolVar(&cfg.Recursive, "r", false, "recursive (short)")
 
 	flag.Parse()
 
-	cfg := Config{}
-
 	if flag.NArg() > 0 {
 		cfg.Dir = flag.Arg(0)
-	}
-
-	cfg.DryRun = *dryRun || *dryRunShort
-	cfg.Quiet = *quiet || *quietShort
-	cfg.Recursive = *recursive || *recursiveShort
-	if *ignoreShort != "" {
-		cfg.Ignore = *ignoreShort
-	} else {
-		cfg.Ignore = *ignore
 	}
 
 	return cfg
